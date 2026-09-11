9206a932ad5ea8bbc136be9b3ccff8095e5590d2
urinlee <urinlee0731@gmail.com>
2026-09-12 02:39:07 +0900

    feat: register 페이지 구현

--- src/app/auth/register.tsx
+++ src/app/auth/register.tsx
@@ -1,10 +1,43 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Screen } from "@/components/Layout/Screen";
+import { BottomButton } from "@/components/UI/BottomButton";
+import { ThemeText } from "@/components/UI/ThemeText";
+import { StyleSheet, View } from "react-native";
 
 export default function Register() {
   return (
-    <SafeAreaView>
-      <Text>Registerfasdf</Text>
-    </SafeAreaView>
+    <Screen>
+      <View style={styles.Container}>
+        <View style={styles.Content}>
+          <ThemeText theme="secondary" style={styles.Description}>
+            서로가 편한 만남이 되는
+          </ThemeText>
+          <ThemeText theme="brand" style={styles.BrandText}>
+            GwiGa
+          </ThemeText>
+        </View>
+        <View style={{ paddingHorizontal: 16, paddingBottom: 64 }}>
+          <BottomButton label="시작하기" onPress={() => {}} />
+        </View>
+      </View>
+    </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    display: "flex",
+  },
+  Content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Description: {
+    fontSize: 12,
+  },
+  BrandText: {
+    fontSize: 72,
+    fontWeight: "bold",
+  },
+});
