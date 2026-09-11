ccadecfeb4573256293fccbc762c14ab86d54784
urinlee <urinlee0731@gmail.com>
2026-09-12 02:39:32 +0900

    feat: 디버깅용 redirect

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,18 @@
 import { Screen } from "@/components/Layout/Screen";
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 export default function Home() {
   return (
     <Screen>
       <Text>Home</Text>
+      <Pressable
+        onPress={() => {
+          router.push("/auth/register");
+        }}
+      >
+        <Text>register</Text>
+      </Pressable>
     </Screen>
   );
 }
