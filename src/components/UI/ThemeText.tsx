e71b69c2477a720100eb0186f3ff3464efdbebc4
urinlee <urinlee0731@gmail.com>
2026-09-12 02:37:37 +0900

    feat: ThemeText 스타일 추가

--- src/components/UI/ThemeText.tsx
+++ src/components/UI/ThemeText.tsx
@@ -1,13 +1,18 @@
 import { Theme } from "@/constants/theme";
 import { useAppTheme } from "@/hooks/useAppTheme";
-import { Text } from "react-native";
+import { StyleProp, Text, TextStyle } from "react-native";
 
 interface ThemeTextProps {
   theme?: keyof Theme["text"];
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  style?: StyleProp<TextStyle>;
 }
 
-export function ThemeText({ theme = "primary", children }: ThemeTextProps) {
+export function ThemeText({
+  theme = "primary",
+  children,
+  style,
+}: ThemeTextProps) {
   const Colors = useAppTheme();
-  return <Text style={{ color: Colors.text[theme] }}>{children}</Text>;
+  return <Text style={[{ color: Colors.text[theme] }, style]}>{children}</Text>;
 }
