cca64f838f2a81030cfdddbd5cd3b8aa28df14d7
urinlee <urinlee0731@gmail.com>
2026-09-15 14:15:04 +0900

    feat: first페이지의 프사 변경 UI 구현(정적)

--- src/app/auth/register/first.tsx
+++ src/app/auth/register/first.tsx
@@ -1,15 +1,33 @@
 import { WelcomeScreen } from "@/components/Layout/WelcomeScreen";
+import { EditableAvatar } from "@/components/UI/Avatar";
 import { BottomButton } from "@/components/UI/BottomButton";
 import { StatusText } from "@/components/UI/StatusText";
 import { ThemeText } from "@/components/UI/ThemeText";
 import { ThemeTextInput } from "@/components/UI/ThemeTextInput";
 import { UserValidation } from "@/constants/validation";
+import { useSinglePage } from "@/hooks/useSinglePage";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
-import { StyleSheet, View } from "react-native";
+import { Keyboard, StyleSheet, View } from "react-native";
+
+const UserNameValidation = UserValidation.pick({ name: true });
 
 export default function first() {
-  const UserNameValidation = UserValidation.pick({ name: true });
+  const { page, setPage, NextPage, PrevPage, pageData } = useSinglePage(
+    0,
+    1,
+    0,
+    {
+      0: {
+        onFocus: () => {},
+      },
+      1: {
+        onFocus: () => {
+          setPage(0);
+        },
+      },
+    },
+  );
   const {
     control,
     handleSubmit,
@@ -19,26 +37,51 @@ export default function first() {
     resolver: zodResolver(UserNameValidation),
   });
 
-  const onSubmit = (data: any) => {
+  const onNextButton = (data: any) => {
     // TODO: 재검사 후 서버 전송
     // 단, 유효성 검사는 클라이언트에서만 하면 안됨
+    Keyboard.dismiss();
+    NextPage();
+  };
+
+  const onPrevButton = () => {
+    PrevPage();
   };
 
   return (
     <WelcomeScreen>
       <View style={styles.Container}>
         <View style={styles.Content}>
-          <ThemeText theme="secondary" style={styles.msg}>
-            닉네임을 알려주세요!
-          </ThemeText>
+          {page === 1 && (
+            <>
+              <ThemeText theme="secondary" style={styles.msg}>
+                프로필 사진도 바꿀 수 있어요{":)"}
+              </ThemeText>
+              <EditableAvatar
+                size={160}
+                uri="https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e66113e2bd2b7407c8202a97d2241a96625"
+              />
+            </>
+          )}
+          {page === 0 && (
+            <ThemeText theme="secondary" style={styles.msg}>
+              닉네임을 알려주세요!
+            </ThemeText>
+          )}
           <Controller
             control={control}
             name={"name"}
             render={({ field: { onChange, value } }) => (
               <ThemeTextInput
-                style={{ width: "80%" }}
+                style={{
+                  width: "80%",
+                  borderWidth: 0,
+                  borderBottomWidth: 1,
+                  textAlign: "center",
+                }}
                 onChangeText={onChange}
                 value={value}
+                onPressIn={pageData?.onFocus}
               />
             )}
           />
@@ -55,7 +98,7 @@ export default function first() {
           <BottomButton
             label="다음"
             disabled={!isValid}
-            onPress={handleSubmit(onSubmit)}
+            onPress={handleSubmit(onNextButton)}
           />
         </View>
       </View>
